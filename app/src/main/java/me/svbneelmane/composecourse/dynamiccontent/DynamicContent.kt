0d9d61f0d52b5a9684d1af42013a92ea70aa48e4
shivaprasad <shivaprasad@svbneelmane.me>
2021-09-11 23:51:29 +0530

    Compose - Dynamic Contents example

--- app/src/main/java/me/svbneelmane/composecourse/dynamiccontent/DynamicContent.kt
+++ app/src/main/java/me/svbneelmane/composecourse/dynamiccontent/DynamicContent.kt
@@ -1,16 +1,21 @@
 package me.svbneelmane.composecourse.dynamiccontent
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
-val nameList: List<String> = listOf("John", "Doe", "Shiva", "Prasad", "Gorge")
+val nameList: ArrayList<String> = arrayListOf("John", "Doe", "Shiva", "Prasad", "Gorge")
 
 class DynamicContent : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,17 +33,28 @@ fun Screen() {
 
 @Composable
 fun GreetingList(names: List<String>) {
-    Column{
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
         // Dynamically add the UI elements
         for (name in names) {
             Greetings(name = name)
+        }
+
+        Button(onClick = {
+            Log.e("ButtonClick","Button Add New Name clicked")
+            nameList.add("New Name")
+        }) {
+            Text(text = "Add New Name")
         }
     }
 }
 
 @Composable
 fun Greetings(name: String) {
-    Text(text = "Hello $name")
+    Text(text = "Hello $name", color = Color.Black)
 }
 
 @Preview(showBackground = true)
